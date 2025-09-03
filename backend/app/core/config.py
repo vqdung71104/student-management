fd1927be449690c2e052658cda7cc937132848dd
Vu Quang Dung <vuquangdung71104@gmail.com>
2025-09-03 22:43:19 +0700

    ..

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,14 +3,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
     PROJECT_NAME: str = "University API"
-    DATABASE_URL: str = os.getenv(
-        "DATABASE_URL", 
-        "mysql+pymysql://root:@localhost:3306/student-management"
-    )
+
+    # Lấy thông tin từ .env
+    MYSQL_USER: str = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD: str = os.getenv("MYSQL_PASSWORD", "123456")
+    MYSQL_HOST: str = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_PORT: str = os.getenv("MYSQL_PORT", "3306")
+    MYSQL_DB: str = os.getenv("MYSQL_DB", "studentmanagemant")
+
+    # Tạo DATABASE_URL động
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"mysql+pymysql://{self.MYSQL_USER}:{self.MYSQL_PASSWORD}"
+            f"@{self.MYSQL_HOST}:{self.MYSQL_PORT}/{self.MYSQL_DB}"
+        )
+
     SECRET_KEY: str = os.getenv("SECRET_KEY", "secret")
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+    HOST: str = os.getenv("HOST", "127.0.0.1")
+    PORT: int = int(os.getenv("PORT", 8000))
+
 
 settings = Settings()
