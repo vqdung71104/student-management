5dccdd29ede10f8fa878d232d9cbffddc8e92d08
Vu Quang Dung <vuquangdung71104@gmail.com>
2025-09-06 23:39:09 +0700

    api students

--- backend/app/models/student_model.py
+++ backend/app/models/student_model.py
@@ -12,17 +12,17 @@ class Student(Base):
     student_name = Column(String(255), nullable=False)
     course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
     enrolled_year = Column(Integer, nullable=False)
-    training_level = Column(String(100), nullable=False)  # enum-like
-    learning_status = Column(String(50), nullable=False)  # enum-like
+    training_level = Column(String(100), nullable=False, default="Cử nhân")  # enum-like
+    learning_status = Column(String(50), nullable=False, default="Đang học")  # enum-like
     gender = Column(String(10), nullable=False)  # enum-like
-    classes = Column(String(500))
+    classes = Column(String(500))  #ví dụ: Việt Nhật 01
     email = Column(String(255), unique=True, nullable=False)  # auto-generate
     newest_semester = Column(String(20))
     cpa = Column(Float, default=0.0)
     failed_subjects_number = Column(Integer, default=0)
     study_subjects_number = Column(Integer, default=0)
-    year_level = Column(String(20))
-    warning_level = Column(String(50))
+    year_level = Column(String(20),default="Trình độ năm 1")  #enum-like
+    warning_level = Column(String(50), default="Cảnh cáo mức 0")  #enum-like
     level_3_warning_number = Column(Integer, default=0)
     department_id = Column(String(50), ForeignKey("departments.id"))
 
