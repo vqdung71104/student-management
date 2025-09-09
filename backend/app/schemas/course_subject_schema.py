34dddde5445968163f7164a0c6ff56fba23bc4b1
Vu Quang Dung <vuquangdung71104@gmail.com>
2025-09-09 10:13:11 +0700

    thêm kỳ học cho các môn

--- backend/app/schemas/course_subject_schema.py
+++ backend/app/schemas/course_subject_schema.py
@@ -7,15 +7,18 @@ class CourseSubjectBase(BaseModel):
 class CourseSubjectCreate(CourseSubjectBase):
     subject_id: int  # Integer ID từ database
     course_id: int
+    learning_semester: int
 
 class CourseSubjectUpdate(BaseModel):
     subject_id: Optional[int] = None
     course_id: Optional[int] = None
+    learning_semester: Optional[int] = None
 
 class CourseSubjectResponse(CourseSubjectBase):
     id: int
     course_id: int
     subject_id: int
+    learning_semester: int
     
    
 
