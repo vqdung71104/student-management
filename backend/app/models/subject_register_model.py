243ed1e4b58df8e88c71c6c6f0a78bc041c8215f
Vu Quang Dung <vuquangdung71104@gmail.com>
2025-09-19 23:18:01 +0700

    sửa lại hiển thị subject_id

--- backend/app/models/subject_register_model.py
+++ backend/app/models/subject_register_model.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from app.db.database import Base
 
 class SubjectRegister(Base):
@@ -10,6 +11,7 @@ class SubjectRegister(Base):
     subject_name = Column(String(255))
     credits = Column(Integer)
     subject_id = Column(Integer, ForeignKey("subjects.id"))
+    register_date = Column(DateTime, default=func.now())
 
     student = relationship("Student", back_populates="subject_registers")
     subject = relationship("Subject", back_populates="subject_registers")
