0c628d0486b391fc41085de13d0bcd8e552256c6
Vu Quang Dung <vuquangdung71104@gmail.com>
2026-04-27 13:22:27 +0700

    xóa bat

--- test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0backend"
-if exist "venv\Scripts\activate.bat" (
-    call venv\Scripts\activate.bat
-) else if exist "venv\bin\activate" (
-    source venv/bin/activate
-)
-pytest %*
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/backend"
-if [ -f "venv/Scripts/activate" ]; then
-    source venv/Scripts/activate
-elif [ -f "venv/bin/activate" ]; then
-    source venv/bin/activate
-fi
-pytest "$@"
